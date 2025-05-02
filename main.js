9291cce706850581855bc64359da5563595eaee4
Suraj Singh <surajsingh7k@gmail.com>
2025-05-02 21:47:14 +0530

    Cleaned up using .gitignore

---
+++ main.js
@@ -0,0 +1,10 @@
+function showSisebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+
+    function hideSidebar() {
+      const sidebar = document.querySelector(".sidebar");
+      sidebar.style.diaplay = "none";
+    }
+  }
+
